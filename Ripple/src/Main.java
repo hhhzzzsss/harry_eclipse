8e81994a6525a3d9eb7c8ff6d9674e7108fa8ec7
Hui Zhou <hzhou@hzsolutions.net>
2015-11-28 14:33:03 -0600

    add .classpath .project

--- Ripple/src/Main.java
+++ Ripple/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				Frame frame = new Frame("Bulls Eye");
+				Frame frame = new Frame("Ripple");
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
 			}
